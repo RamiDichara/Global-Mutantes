ab45db0fb794b5650e36d2cc295deaa9bf707dc5
RamiDichara <ramirodichara123@gmail.com>
2025-11-25 12:07:37 -0300

    Update StatsController.java

--- src/main/java/com/example/MutantesGlobal/controllers/StatsController.java
+++ src/main/java/com/example/MutantesGlobal/controllers/StatsController.java
@@ -1,6 +1,8 @@
 package com.example.MutantesGlobal.controllers;
 
 import com.example.MutantesGlobal.services.StatsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,13 @@ public class StatsController {
     private final StatsService statsService;
 
     @GetMapping("/stats")
+    @Operation(
+            summary = "Obtiene las estadísticas de verificaciones de ADN",
+            description = "Devuelve la cantidad de humanos, mutantes y el ratio entre ambos.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Estadísticas obtenidas correctamente")
+            }
+    )
     public Map<String, Object> getStats() {
         return statsService.getStats();
     }
